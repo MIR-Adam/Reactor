e301732bd86d2dec83fc8853ba4a174999220fd7
Adam Creeger <mikefero@DNA.local>
2019-07-31 15:10:59 -0700

    Initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const apiSections = [
+  {
+    id: 1,
+    title: 'First Section',
+    location: '#1A'
+  },
+  {
+    id: 2,
+    title: 'Second Section',
+    location: '#1B'
+  },
+  {
+    id: 3,
+    title: 'Third Section',
+    location: '#1C'
+  },
+  {
+    id: 4,
+    title: 'Last Section',
+    location: '#2A'
+  }
+]
+function scanFor(userSearch) {
+  return function (result) {
+    return result.title.toLowerCase().includes(userSearch.toLowerCase()) || !userSearch;
+  }
+}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sections: apiSections,
+      userSearch: ''
+    }
+    this.scan = this.scan.bind(this);
+  }
+  scan(event) {
+    this.setState({ userSearch: event.target.value })
+
+  }
+  render() {
+    const { userSearch, sections } = this.state;
+    return (
+      <div className="App">
+        <form>
+          <input 
+          type="text"
+            onChange={this.scan}
+            value={userSearch}
+          />
+        </form>
+        {
+          sections.filter(scanFor(userSearch)).map(section =>
+            <div key={apiSections.id}>
+              <h2>{section.location}{' | '}{section.title}</h2>
+            </div>
+          )
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
